65a6967ca6d6438d326f1d517372431fdec51806
Helryson Rodrigues <106930908+Helryson@users.noreply.github.com>
2023-09-02 18:49:39 -0300

    Adição da função Math e modificaçoes extras

---
+++ Constantes/Constante.js
@@ -0,0 +1,21 @@
+/*
+const PI = 3.14; // Sempre, ao definir uma constante, deixar as letras em maiúsculo.
+let raio = Number(window.prompt("Insira o raio do círculo:"));
+let circunferencia = 2 * raio * PI;
+console.log(circunferencia);
+*/
+
+
+
+
+let x = -4
+
+// x = Math.round(x); // Se estiver na metade, irá arredondar para cima.
+// x = Math.floor(x); //Arredonda para baixo, independente da casa decimal.
+// x = Math.ceil(x); //Arredonda para cima.
+// x = Math.pow(x, 2); // Elevação, 1° numero que vou elevar, 2° expoente.
+// x = Math.sqrt(x); // Raiz quadrada.
+x = Math.abs(x); // Valor absoluto, módulo.
+
+console.log(x);
+

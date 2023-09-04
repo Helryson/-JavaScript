fe8d9508114346d9034524cbec30e2c00a6ecb51
Helryson Rodrigues <106930908+Helryson@users.noreply.github.com>
2023-09-03 22:30:06 -0300

    finalizaçao constantes e Math function

--- Constantes/Constante.js
+++ Constantes/Constante.js
@@ -5,17 +5,24 @@ let circunferencia = 2 * raio * PI;
 console.log(circunferencia);
 */
 
+let z = 3.14;
+let y = 6;
+let x = -4;
 
-
-
-let x = -4
+let maximo;
+let minimo;
 
 // x = Math.round(x); // Se estiver na metade, irá arredondar para cima.
 // x = Math.floor(x); //Arredonda para baixo, independente da casa decimal.
 // x = Math.ceil(x); //Arredonda para cima.
 // x = Math.pow(x, 2); // Elevação, 1° numero que vou elevar, 2° expoente.
 // x = Math.sqrt(x); // Raiz quadrada.
-x = Math.abs(x); // Valor absoluto, módulo.
+// x = Math.abs(x); // Valor absoluto, módulo.
 
-console.log(x);
+maximo = Math.max(x, y, z); // Ira pegar o maior valor entre os 3
+minimo = Math.min(x, y, z);
+
+// x = Math.PI; // x = pi com 15 casas decimais
+
+console.log(max);
 

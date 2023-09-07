e7e46ab0e5bc29af420d20971fd83e7c67ac1818
Helryson Rodrigues <106930908+Helryson@users.noreply.github.com>
2023-09-07 13:02:14 -0300

    Sequência de métodos e slicing

--- String/Metodos.js
+++ String/Metodos.js
@@ -8,8 +8,22 @@ let numero = "12-34567890";
 nome = nome.trim();       //Remove espaçamento antes e depois
 //nome = nome.toLowerCase();     Deixa todas as letras minúsculas.
 //nome = nome.toUpperCase();       Deixa todas as letras maiúsculas.
+//numero = numero.replaceAll("-", "/");
 
-numero = numero.replaceAll("-", "/");
+let primeiroNome;
+let sobrenome;
 
-console.log(nome);
-console.log(numero);
+//primerioNome = nome.slice(0, 5); //Pega desde o index zero ate 1 index antes do 5;
+//sobrenome = nome.slice(6); //Pega do index 6 em diante;
+
+primeiroNome = nome.slice(0, nome.indexOf(" "));
+sobrenome = nome.slice(nome.indexOf(" ") + 1);
+
+console.log(primeiroNome);
+console.log(sobrenome);
+//console.log(nome);
+//console.log(numero);
+
+
+let nick = "     pianta";
+let letra = nick.charAt(0).toUpperCase().trim(); //Modo de chamar um metodo depois do outro.

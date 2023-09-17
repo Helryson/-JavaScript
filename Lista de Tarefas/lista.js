71c9338828dc153acf190cbc49103fce7d430242
Helryson Rodrigues <106930908+Helryson@users.noreply.github.com>
2023-09-17 17:18:55 -0300

    atualização quase completa

--- Lista de Tarefas/lista.js
+++ Lista de Tarefas/lista.js
@@ -1,5 +1,27 @@
+let a, b, c, d, e;
+
 document.getElementById("addTarefa").onclick = function(){
-
-    document.getElementById("tarefa1").innerHTML = 
-
+    if(document.getElementById("tarefa1").innerHTML.trim() === ""){ 
+        a = window.prompt("Adicionar tarefa 1:");
+        document.getElementById("tarefa1").textContent = a;
+    }
+    else if(document.getElementById("tarefa2").innerHTML.trim() === ""){ 
+        b = window.prompt("Adicionar tarefa 2:");
+        document.getElementById("tarefa2").textContent = b;
+    }
+    else if(document.getElementById("tarefa3").innerHTML.trim() === ""){ 
+        c = window.prompt("Adicionar tarefa 3:");
+        document.getElementById("tarefa3").textContent = c;
+    }
+    else if(document.getElementById("tarefa4").innerHTML.trim() === ""){ 
+        d = window.prompt("Adicionar tarefa 4:");
+        document.getElementById("tarefa4").textContent = d;
+    }
+    else if(document.getElementById("tarefa5").innerHTML.trim() === ""){ 
+        e = window.prompt("Adicionar tarefa 5:");
+        document.getElementById("tarefa5").textContent = e;
+    }
+    else{
+        window.alert("Limite de tarefas atingido.\n\nClique em OK para continuar.");
+    }
 }

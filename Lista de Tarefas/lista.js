841429f4d78fd845fd635aefb8e2b14b156b6cd4
Helryson Rodrigues <106930908+Helryson@users.noreply.github.com>
2023-09-17 21:25:37 -0300

    finalização

--- Lista de Tarefas/lista.js
+++ Lista de Tarefas/lista.js
@@ -1,27 +1,40 @@
-let a, b, c, d, e;
-
 document.getElementById("addTarefa").onclick = function(){
     if(document.getElementById("tarefa1").innerHTML.trim() === ""){ 
-        a = window.prompt("Adicionar tarefa 1:");
-        document.getElementById("tarefa1").textContent = a;
+        document.getElementById("tarefa1").textContent = window.prompt("Adicionar tarefa 1:");
     }
     else if(document.getElementById("tarefa2").innerHTML.trim() === ""){ 
-        b = window.prompt("Adicionar tarefa 2:");
-        document.getElementById("tarefa2").textContent = b;
+        document.getElementById("tarefa2").textContent = window.prompt("Adicionar tarefa 2:");
     }
     else if(document.getElementById("tarefa3").innerHTML.trim() === ""){ 
-        c = window.prompt("Adicionar tarefa 3:");
-        document.getElementById("tarefa3").textContent = c;
+        document.getElementById("tarefa3").textContent = window.prompt("Adicionar tarefa 3:");
     }
     else if(document.getElementById("tarefa4").innerHTML.trim() === ""){ 
-        d = window.prompt("Adicionar tarefa 4:");
-        document.getElementById("tarefa4").textContent = d;
+        document.getElementById("tarefa4").textContent = window.prompt("Adicionar tarefa 4:");
     }
     else if(document.getElementById("tarefa5").innerHTML.trim() === ""){ 
-        e = window.prompt("Adicionar tarefa 5:");
-        document.getElementById("tarefa5").textContent = e;
+        document.getElementById("tarefa5").textContent = window.prompt("Adicionar tarefa 5:");
     }
     else{
         window.alert("Limite de tarefas atingido.\n\nClique em OK para continuar.");
     }
+}
+document.getElementById("rt1").onclick = function(){
+    document.getElementById("tarefa1").textContent = "";
+    document.getElementById("marc1").checked = false;
+}
+document.getElementById("rt2").onclick = function(){
+    document.getElementById("tarefa2").textContent = "";
+    document.getElementById("marc2").checked = false;
+}
+document.getElementById("rt3").onclick = function(){
+    document.getElementById("tarefa3").textContent = "";
+    document.getElementById("marc3").checked = false;
+}
+document.getElementById("rt4").onclick = function(){
+    document.getElementById("tarefa4").textContent = "";
+    document.getElementById("marc4").checked = false;
+}
+document.getElementById("rt5").onclick = function(){
+    document.getElementById("tarefa5").textContent = "";
+    document.getElementById("marc5").checked = false;
 }
